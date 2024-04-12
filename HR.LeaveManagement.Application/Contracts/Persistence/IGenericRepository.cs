22c338e69d481937aa774087680599461e8448ad
dev <slawomirek16@gmail.com>
2024-04-12 16:24:27 +0200

    feat: Added application project and repository interfaces

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> GetAsync(int id);
+        Task<T> GetByIdAsync(int id);
+    }
+
+}
