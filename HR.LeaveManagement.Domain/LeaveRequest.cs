ed96777420b2b2b238e554bde11e92830ef862e5
dev <slawomirek16@gmail.com>
2024-04-14 20:45:34 +0200

    refactor: corrected wrong name

--- HR.LeaveManagement.Domain/LeaveRequest.cs
+++ HR.LeaveManagement.Domain/LeaveRequest.cs
@@ -10,7 +10,7 @@ namespace HR.LeaveManagement.Domain
         [ForeignKey("LeaveTypeId")]
         public LeaveType? LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
-        public DateTime DateReuested { get; set; }
+        public DateTime DateRequested { get; set; }
         public string? RequestComments { get; set; }
         public bool? Approved { get; set; }
         public bool Canceled { get; set; }
