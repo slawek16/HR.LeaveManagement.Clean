f2848ec2488bea49fb4d0fb17915ca89ee3dbb84
dev <slawomirek16@gmail.com>
2024-04-22 10:25:36 +0200

    feat: added persistence project

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistentServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
