84972a33bad099d7fa47be260c9c1a14ba71435d
dev <slawomirek16@gmail.com>
2024-04-23 09:42:11 +0200

    feat: set up email sender service

---
+++ HR.LeaveManagement.Application/Model/EmailMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Application.Model
+{
+    public class EmailMessage
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
