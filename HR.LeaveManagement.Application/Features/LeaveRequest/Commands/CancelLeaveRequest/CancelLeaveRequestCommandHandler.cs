92a363147e803008fa632788a23fc2894c61c133
dev <slawomirek16@gmail.com>
2024-04-29 16:54:25 +0200

    feat: added cancell leaverequest command

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CancelLeaveRequest/CancelLeaveRequestCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Model;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CancelLeaveRequest
+{
+    public class CancelLeaveRequestCommandHandler : IRequestHandler<CancelLeaveRequestCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IEmailSender _emailSender;
+        private readonly IAppLogger<CancelLeaveRequestCommand> _logger;
+
+        public CancelLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IEmailSender emailSender, IAppLogger<CancelLeaveRequestCommand> logger)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+        public async Task<Unit> Handle(CancelLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            Domain.LeaveRequest leaveRequest = await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id);
+            if (leaveRequest == null)
+                throw new NotFoundException(nameof(leaveRequest), request.Id);
+
+            leaveRequest.Canceled = true;
+
+            await _leaveRequestRepository.UpdateAsync(leaveRequest);
+
+            try
+            {
+                EmailMessage emailMessage = new EmailMessage
+                {
+                    To = string.Empty,
+                    Body = "Your leave request has been cancelled.",
+                    Subject = "Leave Request Cancelled."
+                };
+
+                await _emailSender.SendEmail(emailMessage);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex.Message, ex);
+            }
+
+            return Unit.Value; 
+        }
+    }
+}
