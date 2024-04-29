829474bc6acf2aae7e6982663fa644e1b14f9e5f
dev <slawomirek16@gmail.com>
2024-04-29 09:48:21 +0200

    feat: leave allocation delete command added

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/DeleteLeaveAllocation/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation
+{
+    public class DeleteLeaveAllocationCommandHandler : IRequestHandler<DeleteLeaveAllocationCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+        public DeleteLeaveAllocationCommandHandler(IMapper mapper, ILeaveAllocationRepository leaveAllocationRepository)
+        {
+            _mapper = mapper;
+            _leaveAllocationRepository = leaveAllocationRepository;
+        }
+        public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            DeleteLeaveAllocationCommandValidator validations = new DeleteLeaveAllocationCommandValidator(_leaveAllocationRepository);
+            var validationResults = await validations.ValidateAsync(request, cancellationToken);
+            if (validationResults.Errors.Any())
+                throw new BadRequestException("Delete leave allocation error", validationResults);
+
+            var leaveAllocation = _mapper.Map<Domain.LeaveAllocation>(request);
+            await _leaveAllocationRepository.DeleteAsync(leaveAllocation);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/DeleteLeaveAllocation/DeleteLeaveAllocationCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.DeleteLeaveAllocation
+{
+    public class DeleteLeaveAllocationCommandValidator : AbstractValidator<DeleteLeaveAllocationCommand>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+
+        public DeleteLeaveAllocationCommandValidator(ILeaveAllocationRepository leaveAllocationRepository)
+        {
+            RuleFor(q => q.Id)
+                .MustAsync(LeaveAllocationExists)
+                .WithMessage("Leave Allocation does not exist in database.");
+
+            _leaveAllocationRepository = leaveAllocationRepository;
+        }
+
+        private async Task<bool> LeaveAllocationExists(int id, CancellationToken token)
+        {
+            var allocation = await _leaveAllocationRepository.GetLeaveAllocationWithDetails(id);
+            return allocation != null;
+        }
+    }
+}
