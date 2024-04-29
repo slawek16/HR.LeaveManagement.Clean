5f114116ce490d5cae44e376d936717343cba858
dev <slawomirek16@gmail.com>
2024-04-29 17:21:23 +0200

    feat: added controller for leaverequest

---
+++ HR.LeaveManagement.Api/Controllers/LeaveRequestController.cs
@@ -0,0 +1,92 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CancelLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.ChangeLeaveRequestApproval;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.DeleteLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveRequestController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveRequestController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        // GET: api/<LeaveRequestController>
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveRequestListDto>>> Get()
+        {
+            var leaveRequest = await _mediator.Send(new GetLeaveRequestListQuery());
+            return Ok(leaveRequest);
+        }
+
+        // GET api/<LeaveRequestController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveRequestDetailDto>> Get(int id)
+        {
+            var leaveRequest = await _mediator.Send(new GetLeaveRequestDetailQuery(id));
+            return Ok(leaveRequest);
+        }
+
+        // POST api/<LeaveRequestController>
+        [HttpPut]
+        [Route("CancelRequest")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+
+        public async Task<ActionResult> CancelRequest(CancelLeaveRequestCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Route("UpdateApproval")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+
+        public async Task<ActionResult> UpdateApproval(ChangeLeaveRequestApprovalCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // PUT api/<LeaveRequestController>/5
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Put(UpdateLeaveRequestCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // DELETE api/<LeaveRequestController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteLeaveRequestCommand(id));
+            return NoContent();
+        }
+    }
+}
