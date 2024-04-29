99f8cf6415fafa42453b312ebd606dac9616592a
dev <slawomirek16@gmail.com>
2024-04-29 16:06:04 +0200

    refactor: added abstract class for leaverequest commands

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/UpdateLeaveRequest/UpdateLeaveRequestCommand.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveRequest.Shared;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.UpdateLeaveRequest
+{
+    public class UpdateLeaveRequestCommand : BaseLeaveRequest
+    {
+        public int Id { get; set; }
+        public string RequestCommand { get; set; } = string.Empty;
+        public bool Cancelled { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Shared/BaseLeaveRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Shared
+{
+    public class BaseLeaveRequestValidator : AbstractValidator<BaseLeaveRequest>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public BaseLeaveRequestValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            RuleFor(p => p.StartDate)
+                .LessThan(p => p.EndDate).WithMessage("{PropertyName} must be before {ComparisionValue}");
+
+            RuleFor(p => p.EndDate)
+                .GreaterThan(p => p.StartDate).WithMessage("{PropertyName} must be after {ComparisionValue}");
+
+            RuleFor(p => p.LeaveTypeId)
+                .GreaterThan(0)
+                .MustAsync(LeaveTypeMustExist).WithMessage("{PropertyName} does not exist.");
+        }
+
+        private async Task<bool> LeaveTypeMustExist(int id, CancellationToken token)
+        {
+            return await _leaveTypeRepository.IsLeaveTypeExist(id); 
+        }
+    }
+}
