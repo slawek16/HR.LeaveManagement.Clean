96742ee9440e10c0010d842a165d7a2f593cfaf4
dev <slawomirek16@gmail.com>
2024-04-29 15:36:14 +0200

    feat: added queries of leaverequest list

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestList/GetLeaveRequestListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList
+{
+    public class GetLeaveRequestListQueryHandler : IRequestHandler<GetLeaveRequestListQuery, List<LeaveRequestListDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public GetLeaveRequestListQueryHandler(IMapper mapper, ILeaveRequestRepository leaveRequestRepository)
+        {
+            _mapper = mapper;
+            _leaveRequestRepository = leaveRequestRepository;
+        }
+        public async Task<List<LeaveRequestListDto>> Handle(GetLeaveRequestListQuery request, CancellationToken cancellationToken)
+        {
+            List<Domain.LeaveRequest> leaveRequests = await _leaveRequestRepository.GetLeaveRequestsWithDetails();
+            List<LeaveRequestListDto> leaveRequestListDtos = _mapper.Map<List<LeaveRequestListDto>>(leaveRequests);
+            return leaveRequestListDtos;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveRequestProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.MappingProfiles
+{
+    public class LeaveRequestProfile : Profile
+    {
+        public LeaveRequestProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+        }
+    }
+}
