a6b30da1e09830bf72466cdf501c8f90d2802068
dev <slawomirek16@gmail.com>
2024-05-03 10:24:41 +0200

    feat: added custom error handling

---
+++ HR.LeaveManagement.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,71 @@
+﻿using HR.LeaveManagement.Api.Middleware.Models;
+using System.Net;
+using BadRequestException = HR.LeaveManagement.Application.Exceptions.BadRequestException;
+using NotFoundException = HR.LeaveManagement.Application.Exceptions.NotFoundException;
+
+namespace HR.LeaveManagement.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            CustomProblemDetails problem = new();
+
+            switch (ex)
+            {
+                case BadRequestException badRequestException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    problem = new CustomProblemDetails
+                    {
+                        Title = badRequestException.Message,
+                        Status = (int)statusCode,
+                        Detail = badRequestException.InnerException?.Message,
+                        Type = nameof(BadRequestException),
+                        Errors = badRequestException.ValidationErrors
+                    };
+                    break;
+                case NotFoundException NotFound:
+                    statusCode = HttpStatusCode.NotFound;
+                    problem = new CustomProblemDetails
+                    {
+                        Title = NotFound.Message,
+                        Status = (int)statusCode,
+                        Detail = NotFound.InnerException?.Message,
+                        Type = nameof(NotFoundException),
+                    };
+                    break;
+                default:
+                    problem = new CustomProblemDetails
+                    {
+                        Title = ex.Message,
+                        Status = (int)statusCode,
+                        Detail = ex.StackTrace,
+                        Type = nameof(HttpStatusCode.InternalServerError),
+                    };
+                    break;
+            }
+            httpContext.Response.StatusCode = (int)statusCode;
+            await httpContext.Response.WriteAsJsonAsync(problem);
+        }
+    }
+}
