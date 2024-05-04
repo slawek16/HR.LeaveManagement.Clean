10b212ddcc8f9404834c49ddaf192bc7d7d6365d
dev <slawomirek16@gmail.com>
2024-05-04 20:22:12 +0200

    feat: introduced basic contracts and services

--- HR.LeaveManagement.BlazorUI/Program.cs
+++ HR.LeaveManagement.BlazorUI/Program.cs
@@ -1,4 +1,7 @@
 using HR.LeaveManagement.BlazorUI;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services;
+using HR.LeaveManagement.BlazorUI.Services.Base;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7041"));
+builder.Services.AddScoped<ILeaveTypeService, LeaveTypeService>();
+builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
+builder.Services.AddScoped<ILeaveAllocationService, LeaveAllocationService>();
 
 await builder.Build().RunAsync();
