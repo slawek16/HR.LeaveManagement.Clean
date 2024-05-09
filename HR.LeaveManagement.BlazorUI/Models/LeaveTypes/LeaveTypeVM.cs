2572a1b9258013e3957cdbb3dc6be9f15527a079
dev <slawomirek16@gmail.com>
2024-05-09 10:21:34 +0200

    feat: setting up LeaveTypeService

---
+++ HR.LeaveManagement.BlazorUI/Models/LeaveTypes/LeaveTypeVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagement.BlazorUI.Models.LeaveTypes
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name ="DefaultNumberOfDays")]
+        public string DefaultDays { get; set; }
+    }
+}
--- HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
+++ HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
@@ -7,5 +7,30 @@
         {
             _client = client;
         }
+        protected Response<Guid> ConvertApiExceptions<Guid>(ApiException ex)
+        {
+            switch (ex.StatusCode)
+            {
+                case 400:
+                    return new Response<Guid>()
+                    {
+                        Message = "Invalid data  was submitted.",
+                        ValidationErrors = ex.Response,
+                        Success = true
+                    };
+                case 404:
+                    return new Response<Guid>()
+                    {
+                        Message = "The record was not found",
+                        Success = false
+                    };
+                default:
+                    return new Response<Guid>()
+                    {
+                        Message = "Something went wrong, please try again later.",
+                        Success = true
+                    };
+            }
+        }
     }
 }
